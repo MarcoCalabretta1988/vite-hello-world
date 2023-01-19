5fc2e251dd442ff4c3ec24b4b70f4a3efc1deb8a
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-19 17:24:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { AppTitle }
+}
+</script>
+<template>
+  <app-title> </app-title>
+</template>
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Vite first use'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
