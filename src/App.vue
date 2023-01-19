918b8cbbe17ec4220bea74d72dab24c418650800
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-19 17:30:55 +0100

    Add arcoded img

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
+import AppHero from './components/AppHero.vue';
 import AppTitle from './components/AppTitle.vue';
 
 export default {
   data() {
     return {};
   },
-  components: { AppTitle }
+  components: { AppTitle, AppHero }
 }
 </script>
 <template>
-  <app-title> </app-title>
+  <app-title></app-title>
+  <app-hero></app-hero>
 </template>
 <style>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <img src="../assets/banner.png" alt="">
+</template>
